a498340f184ce4b1c45879379e81c1759f0f0163
Daniel Jurek <daniel@maillift.com>
2013-12-05 20:40:15 -0800

    And we’re at v1. Now to get into UI’s

--- platforms/ios/www/js/followup.js
+++ platforms/ios/www/js/followup.js
@@ -1,6 +1,8 @@
 var FollowUp = { 
 
-	domain: "http://followup.danieljurek.com", 
+	domain: "http://followup.danieljurek.com",			// Leave out the trailing / 
+	cameraQuality: 50, 
+	checkServerDelay: 5000, 
 
 	init: function() { 
 		//
@@ -8,25 +10,21 @@ var FollowUp = {
 		//
 		var userId = localStorage.getItem("userId"); 
 		if(userId) { 
-			window.location.hash = "main"; 
-			setTimeout(FollowUp.checkServerActions, 2000); 
+			window.location.hash = "main";  
+			setTimeout(FollowUp.checkServerActions, FollowUp.checkServerDelay); 
 		} else { 
 			window.location.hash = "login"; 
 		}
-
 
 		//
 		//	Events
 		//
 		$("#loginButton").click(FollowUp.authenticate); 
-		//$("#newFollowUpButton").click(FollowUp.)
 		$("#editCampaignButton").click(FollowUp.showEditCampaign); 
 
 		//
 		//	Check server for contact status
 		//
-		
-
 	}, 
 
 
@@ -56,32 +54,65 @@ var FollowUp = {
 			data: { user_id: FollowUp.fromLocal("userId") }, 
 			success: function(data) { 
 				var cards = FollowUp.fromLocal("cards"); 
+				if(!cards) 
+						cards = {}; 
 				var updateTables = false; 
 
 				$.each(data, function(index, value) { 
-					if(cards[index]) { 
-						cards[index].email 			= value.contact.emails[0].value; 
-						cards[index].firstName 		= value.contact.name.givenName; 
-						cards[index].lastName 		= value.contact.name.familyName; 
-						cards[index].organization 	= value.contact.organizations[0].name; 
-
-						cards[index].read 			= (value.views > 0 ? true : false); 
-
+					if(!cards[index]) { 
+						cards[index] = {}; 
 						updateTables = true; 
+					}
+
+					if(cards[index].email != value.contact.emails[0].value) {
+						cards[index].email = value.contact.emails[0].value; 
+						updateTables = true; 
+					}
+
+					if(cards[index].firstName != value.contact.name.givenName) {
+						cards[index].firstName = value.contact.name.givenName; 
+						updateTables = true; 
+					}
+
+					if(cards[index].lastName != value.contact.name.familyName) {
+						cards[index].lastName = value.contact.name.familyName; 
+						updateTables = true; 
+					}
+
+					if(cards[index].organization != value.contact.organizations[0].name) { 
+						cards[index].organization = value.contact.organizations[0].name;
+						updateTables = true; 
+					} 
+
+					if(cards[index].read != (value.views > 0 ? true : false)) { 
+						cards[index].read = (value.views > 0 ? true : false); 
+						updateTables = true; 
+					}
+
+					if(cards[index].campaign_id != value.campaignId) { 
+						cards[index].campaign_id = value.campaignId; 
+					}
+
+					if(cards[index].track != value.track) { 
+						cards[index].track = value.track; 
+					}
+
+					if(cards[index].sent != value.sent) { 
+						cards[index].sent = value.sent; 
 					}
 				}); 
 
 				FollowUp.toLocal("cards", cards); 			
 				
-				if(updateTables)
+				if(updateTables) 
 					FollowUp.loadViewContacts(); 
 
 			}, 
 			error: function() { 
-				alert("There was an error getting status data from the server"); 
+				// Sink error 
 			}, 
 			complete: function() { 
-				setTimeout(FollowUp.checkServerActions, 2000); 
+				setTimeout(FollowUp.checkServerActions, FollowUp.checkServerDelay); 
 			}
 		});
 	}, 
@@ -94,11 +125,11 @@ var FollowUp = {
 		$.ajax(FollowUp.domain + "/user/login", { 
 			type: "POST", 
 			dataType: "json", 
-			data: { email: $("#loginEmailInput").val() }, 
+			data: { email: $("#loginEmailInput").val(), password: $("#loginPasswordInput").val() }, 
 			success: function(data) { 
 				localStorage.setItem("userId", data.user_id); 
 				window.location.hash = "main"; 
-				setTimeout(FollowUp.checkServerActions, 2000); 
+				setTimeout(FollowUp.checkServerActions, FollowUp.checkServerDelay); 
 			}, 
 			error: function() { 
 				alert("There was an error logging in!"); 
@@ -111,17 +142,25 @@ var FollowUp = {
 	},
 
 	populateCampaigns: function(targetId) { 
-		var campaigns = FollowUp.fromLocal("campaigns"); 
+		$.ajax(FollowUp.domain + "/campaign", { 
+			type: 'GET',
+			dataType: 'json', 
+			data: { user_id: FollowUp.fromLocal('userId') }, 
+			success: function(data) { 
+				if(!data.campaigns) 
+					return; 
 
-		if(!campaigns) 
-			return; 
+				$(targetId).html(''); 
 
-		$(targetId).html(''); 
-
-		$.each(campaigns, function(index, value) { 
-			$(targetId).append("<li><a rel='"+ index + "' onClick='FollowUp.handleSelectedCampaign(this)'>" + value.name + "</a></li>"); 
-		});  
-		$(targetId).listview("refresh"); 
+				$.each(data.campaigns, function(index, value) { 
+					$(targetId).append("<li><a rel='"+ value.id + "' onClick='FollowUp.handleSelectedCampaign(this)'>" + value.name + "</a></li>"); 
+				});  
+				$(targetId).listview().listview('refresh'); 
+			}, 
+			error: function() { 
+				alert("There was an error getting campaigns!"); 
+			}
+		}); 
 	}, 
 
 	addCampaignToken: function(element, targetId) { 
@@ -132,19 +171,18 @@ var FollowUp = {
 	}, 
 
 	saveCampaign: function() { 
-		var campaigns = FollowUp.fromLocal("campaigns"); 
-
-		if(!(campaigns instanceof Array))
-			campaigns = new Array(); 
-
-
-		campaigns.push({ 
-			name: $("#campaignName").val(),
-			subject: $("#campaignSubject").val(),
-			body: $("#campaignText").val()
+		$.ajax(FollowUp.domain + "/campaign/create?user_id=" + FollowUp.fromLocal('userId'), { 
+			type: 'POST', 
+			data: { 
+				name: $("#campaignName").val(),
+				subject: $("#campaignSubject").val(),
+				body: $("#campaignText").val()
+			}, 
+			async: false, 
+			error: function() { 
+				alert("There was an error saving the campaign."); 
+			}
 		}); 
-
-		FollowUp.toLocal('campaigns', campaigns); 
 
 		$("#newCampaignForm")[0].reset(); 
 		window.location.hash = "editCampaigns"; 
@@ -152,10 +190,11 @@ var FollowUp = {
 	}, 
 
 	handleSelectedCampaign: function(element) { 
+		debugger; 
 		FollowUp.toLocal("selectedCampaign", $(element).attr('rel')); 
 
 		if(window.location.hash == "#selectCampaign") { 
-			window.location.hash = "newFollowUp"; 
+			FollowUp.captureImage(); 
 		} else {
 			alert("Campaign editing not implemented yet."); 
 		}
@@ -163,69 +202,67 @@ var FollowUp = {
 
 	showSelectCampaign: function() { 
 		FollowUp.populateCampaigns("#selectCampaignList"); 
+		$("#selectCampaignList").listview().listview("refresh"); 
 	},
 
 	captureImage: function() { 
-		window.location.hash = "loadingPage"; 
-
-		$.ajax(FollowUp.domain + '/card/submit', { 
-			type: "POST", 
-			data: { user_id: FollowUp.fromLocal("userId"), front: samplePicture }, 
-			dataType: "json", 
-			success: function(data) { 
-				var cards = FollowUp.fromLocal("cards"); 
-				if(!cards) { 
-					cards = new Object(); 
-				}
-
-				cards[data.request_id] = { 
-					requestId: data.request_id, 
-					email: null, 
-					firstName: null, 
-					lastName: null, 
-					organization: null, 
-					campaign: parseInt(FollowUp.fromLocal('selectedCampaign')),
-					track: ($("#trackEmailSelector").val() == "true" ? true : false), 
-					sent: false, 
-					read: false
-				}; 
-
-				FollowUp.toLocal("cards", cards); 
-
-			}, 
-			error: function() { 
-				alert("There was an error! :(");
-			}, 
-			complete: function() { 
-				window.location.hash = "main";  
+		navigator.camera.getPicture(
+			FollowUp.imageCaptured, 
+			FollowUp.imageCaptureError, 
+			{
+				quality: FollowUp.cameraQuality, 
+				encodingType: Camera.EncodingType.JPEG,
+				destionationType: Camera.DestinationType.FILE_URI, 
+				saveToPhotoAlbum: true, 
+				correctOrientation: true, 
 			}
-		});
-		/*
-		navigator.camera.getPicture(FollowUp.imageCaptured, FollowUp.imageCaptureError, { 
-			quality: 75, 
-			destionationType: Camera.DestinationType.DATA_URL, 
-			saveToPhotoAlbum: true
-		}); */
+		); 
 	}, 
 
 	imageCaptured: function(imageData) { 
+		// File upload example from: http://metabates.com/2012/04/15/capturing-and-uploading-photos-on-ios-with-phonegap/
+      	options = new FileUploadOptions();
+      	options.fileKey = "front";
+      	options.fileName = imageData.substr(imageData.lastIndexOf('/') + 1);
+      	options.mimeType = "image/jpeg";
+      	var params = { user_id: FollowUp.fromLocal('userId'), campaign_id: FollowUp.fromLocal('selectedCampaign') }; 
+      	options.params = params; 
 
+  		uploader = new FileTransfer();
+		uploader.upload(
+			imageData, 
+			FollowUp.domain + '/card/submit?user_id=' + FollowUp.fromLocal('userId'), 
+			function() { 
+				window.location.hash="main"; 
+			}, 
+			function() { 
+				alert("file upload fail! :("); 
+				window.location.hash = 'main'; 
+			}, 
+			options);
+
+		window.location.hash = 'main'; 
 	}, 
+
+	imageCaptureError: function() { 
+		// Error silently 
+	},
 
 	loadViewContacts: function() { 
 		var contacts = FollowUp.fromLocal("cards"); 
 		
-		var contactsTranslating = new Object; 
-		var contactsPendingEmail = new Object; 
-		var contactsWaitingRead = new Object; 
-		var contactsRead = new Object; 
-		var contactNotTracked = new Object; 
+		var contactsTranslating = {}; 
+		var contactsPendingEmail = {}; 
+		var contactsWaitingRead = {}; 
+		var contactsRead = {}; 
+		var contactNotTracked = {}; 
 
 
+		// Sort each card item into its respective list 
 		$.each(contacts, function(index, value) { 
 			if(!value.email) { 
 				contactsTranslating[index] = value; 
-			} else if(!value.sent) { 
+			} else if(value.sent == 0) { 
 				contactsPendingEmail[index] = value; 
 			} else if(value.sent && value.track && !value.read) { 
 				contactsWaitingRead[index] = value; 
@@ -236,81 +273,98 @@ var FollowUp = {
 			}
 		});
 
-		$("#contactsTranslating").html(''); 
+		/*$("#contactsTranslating").html(''); 
 		$.each(contactsTranslating, function(index, value) { 
 			$("#contactsTranslating").append($("<li><a data-role='button'>" + value.requestId + "</a></li>")); 
-		});
-		$("#contactsTranslating").listview("refresh"); 
+			$("#contactsTranslating").listview().listview("refresh"); 
+		});*/
+		
 
 
 		$("#contactsPendingEmail").html(''); 
 		$.each(contactsPendingEmail, function(index, value) { 
 			$("#contactsPendingEmail").append($("<li><a data-role='button' rel='" + index + "' onClick='FollowUp.composeEmail(this)'>" 
 				+ value.firstName + ' ' + value.lastName + "</a></li>")); 
+			$("#contactsPendingEmail").listview().listview("refresh"); 
 		});
-		$("#contactsPendingEmail").listview("refresh"); 
+		
 
 
 		$("#contactsWaitingRead").html(''); 
 		$.each(contactsWaitingRead, function(index, value) { 
 			$("#contactsWaitingRead").append($("<li><a data-role='button'>" + value.firstName + ' ' + value.lastName + "</a></li>")); 
+			$("#contactsWaitingRead").listview().listview("refresh"); 
 		});
-		$("#contactsWaitingRead").listview("refresh"); 
+		
 
 		$("#contactsRead").html(''); 
 		$.each(contactsRead, function(index, value) { 
 			$("#contactsRead").append($("<li><a data-role='button'>" + value.firstName + ' ' + value.lastName + "</a></li>")); 
+			$("#contactsRead").listview().listview("refresh"); 
 		});
-		$("#contactsRead").listview("refresh"); 
+		
 
 
 		$("#contactNotTracked").html(''); 
 		$.each(contactNotTracked, function(index, value) { 
 			$("#contactNotTracked").append($("<li><a data-role='button'>" + value.firstName + ' ' + value.lastName + "</a></li>")); 
+			$("#contactNotTracked").listview().listview("refresh"); 
 		});
-		$("#contactNotTracked").listview("refresh"); 
+		
 
 	}, 
 
-	composeEmail:function(element) {
-		debugger; 
+	composeEmail: function(element) {
 		var cardId = $(element).attr('rel'); 
 		var cards = FollowUp.fromLocal('cards'); 
 		var campaigns = FollowUp.fromLocal('campaigns'); 
 
 		if(cards[cardId]) { 
 			var card = cards[cardId]; 
-			var campaign = campaigns[card.campaign];
 
-			var subject = FollowUp.replaceAll('{FirstName}', card.firstName, campaign.subject); 
-			subject = FollowUp.replaceAll('{LastName}', card.lastName, subject); 
-			subject = FollowUp.replaceAll('{Organization}', card.organization, subject); 
+			var campaign = {}; 
+			$.ajax(FollowUp.domain + '/campaign/get/' + card.campaign_id, { 
+				type: 'GET', 
+				dataType: 'json', 
+				data: { user_id: FollowUp.fromLocal('userId') }, 
+				success: function(data) { 
 
-			var body = FollowUp.replaceAll('{FirstName}', card.firstName, campaign.body); 
-			body = FollowUp.replaceAll('{LastName}', card.lastName, body); 
-			body = FollowUp.replaceAll('{Organization}', card.organization, body); 
+					var campaign = data.campaign; 
+
+					var subject = FollowUp.replaceAll('{FirstName}', card.firstName, campaign.subject); 
+					subject = FollowUp.replaceAll('{LastName}', card.lastName, subject); 
+					subject = FollowUp.replaceAll('{Organization}', card.organization, subject); 
+
+					var body = FollowUp.replaceAll('{FirstName}', card.firstName, campaign.copy); 
+					body = FollowUp.replaceAll('{LastName}', card.lastName, body); 
+					body = FollowUp.replaceAll('{Organization}', card.organization, body); 
 
 
-			if(card.track) { 
-				$.ajax(FollowUp.domain + "/email/create/", { 
-					type: "POST", 
-					dataType: "json", 
-					data: { user_id: FollowUp.fromLocal('userId'), request_id: cardId }, 
-					success: function(data) { 
-						body = FollowUp.replaceAll("\n", "<br />", body); 
-						body += "<img src='"+ FollowUp.domain + "/email/view/?" + data.uuid + "' />"; 
+					if(card.track) { 
+						$.ajax(FollowUp.domain + "/email/create/", { 
+							type: "POST", 
+							dataType: "json", 
+							data: { user_id: FollowUp.fromLocal('userId'), request_id: cardId }, 
+							success: function(data) { 
+								body = FollowUp.replaceAll("\n", "<br />", body); 
+								body += "<img src='"+ FollowUp.domain + "/email/view/" + data.uuid + "' />"; 
 
-						FollowUp.emailComposer(card.email, subject, body, true, cardId);   
-					}, 
-					error: function() { 
-						alert("Error getting the tracking element."); 
-					}
-				}); 
+								FollowUp.emailComposer(card.email, subject, body, true, cardId);   
+							}, 
+							error: function() { 
+								alert("Error getting the tracking element."); 
+							}
+						}); 
 
-				
-			} else { 
-				FollowUp.emailComposer(card.email, subject, body, false, cardId); 	   
-	   		}  
+						
+					} else { 
+						FollowUp.emailComposer(card.email, subject, body, false, cardId); 	   
+			   		}
+				}, 
+				error: function() { 
+					alert("Could not get campaign data."); 
+				}
+			});
 	   }     
 	}, 
 
@@ -325,6 +379,12 @@ var FollowUp = {
 		        	var cards = FollowUp.fromLocal('cards'); 
 		        	cards[cardId].sent = true;
 		        	FollowUp.toLocal('cards', cards); 
+
+		        	$.ajax(FollowUp.domain + '/card/set_sent/' + cardId, { 
+		        		type: 'GET', 
+		        		data: { user_id: FollowUp.fromLocal('userId') }
+		        	}); 
+
 		        } else { 
 		        	alert("email was not sent"); 
 		        }
